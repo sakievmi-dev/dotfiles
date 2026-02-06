6aedec3df534eeeebc3283468f1c95a5964bc5d0
sakievmi <sakievmi@proton.me>
2026-02-06 23:56:01 +0200

    niri: update dependencies in preinstall script

--- niri/preinstall.sh
+++ niri/preinstall.sh
@@ -1,1 +1,2 @@
-sudo pacman -S --noconfirm --needed niri xwayland-satellite xdg-desktop-portal-gnome xdg-desktop-portal-gtk
+sudo pacman -S --noconfirm --needed niri xwayland-satellite xdg-desktop-portal-gnome xdg-desktop-portal-gtk noto-fonts noto-fonts-cjk
+yay --diffmenu=false --cleanmenu=false --editmenu=false -S --noconfirm --needed ttf-twemoji-color bibata-cursor-theme
