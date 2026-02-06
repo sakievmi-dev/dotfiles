5509c03a4b4eda069476336ab13b9172220c8286
sakievmi <sakievmi@proton.me>
2026-02-07 00:15:22 +0200

    yazi: update dependecies in preinstall script

--- yazi/preinstall.sh
+++ yazi/preinstall.sh
@@ -1,2 +1,2 @@
-sudo pacman -S --noconfirm --needed yazi
-yay --diffmenu=false --cleanmenu=false --editmenu=false -S --noconfirm --needed xdg-desktop-portal-termfilechooser-hunkyburrito-git
+sudo pacman -S --noconfirm --needed yazi neovim
+yay --diffmenu=false --cleanmenu=false --editmenu=false -S --noconfirm --needed xdg-desktop-portal-termfilechooser-hunkyburrito-git zen-browser-bin
