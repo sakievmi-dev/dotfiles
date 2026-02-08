9402636a34178b97e5b451aeb35a8742ac002ddd
sakievmi <sakievmi@proton.me>
2026-02-08 15:22:50 +0200

    waybar: add wrapper for customizer script

---
+++ waybar/.local/scripts/waybar-customizer-script-wrapper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+customizer_script=~/.local/scripts/waybar-change-modules-right.py
+
+CHOICE=$(gum choose --item.foreground 250 "Change modules on the right side" "Change modules on the left side (WIP)" "Exit")
+
+case $CHOICE in
+	"Change modules on the right side")
+	$customizer_script -m
+	modules=$(gum input --placeholder "Enter modules separated by ';'. Example: 'niri/language;tray'")
+
+	echo $modules
+
+	$customizer_script --modules_right $modules
+	;;
+esac
