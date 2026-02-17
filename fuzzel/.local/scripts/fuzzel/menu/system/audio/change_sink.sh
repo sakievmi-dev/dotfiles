660f293583cb845b527ccd6e219b3383b992225a
sakievmi <sakievmi@proton.me>
2026-02-17 17:17:58 +0200

    fuzzel, audio: fix sink switching
    options for fuzzel now directly store sink name. script doesn't try to
    guess the name of the sink like it was in the past.

--- fuzzel/.local/scripts/fuzzel/menu/system/audio/change_sink.sh
+++ fuzzel/.local/scripts/fuzzel/menu/system/audio/change_sink.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-SINK_LIST=$(pactl list sinks | perl -00 -ne 'if (/Description:\s*(.+)/) { print "  󱡬 $1\n" }')
+SINK_LIST=$(pactl list sinks | perl -00 -ne 'if (/Name: (.+?)\s+Description: (.+?)$/m) { print "󱡬 $2                                 | $1\n" }')
 MENU_ITEMS=" Back"$'\n'"$SINK_LIST"
-SELECTED_DESC=$(echo "$MENU_ITEMS" | fuzzel -d -p "󱡬 " | sed 's/^\s*//')
+SELECTED_LINE=$(echo "$MENU_ITEMS" | fuzzel -d -p "󱡬 ")
 
-if [[ "$SELECTED_DESC" == " Back" || -z "$SELECTED_DESC" ]]; then
-    [[ "$SELECTED_DESC" == " Back" ]] && bash ~/.local/scripts/fuzzel/menu/system/audio.sh
+if [[ "$SELECTED_LINE" == " Back" || -z "$SELECTED_LINE" ]]; then
+    [[ "$SELECTED_LINE" == " Back" ]] && bash ~/.local/scripts/fuzzel/menu/system/audio.sh
     exit 0
 fi
 
-CLEARED_DESC=$(echo "$SELECTED_DESC" | sed 's/^󱡬 //')
-SINK_NAME=$(pactl list sinks | perl -00 -ne "if (/Description: \Q$CLEARED_DESC\E/m && /Name: (.+)/m) { print \$1 }")
+SINK_NAME=$(echo "$SELECTED_LINE" | awk -F ' | ' '{print $NF}' | xargs)
 
 if [ -n "$SINK_NAME" ]; then
     pactl set-default-sink "$SINK_NAME"
 
-    # Moving all streams to active sink
     pactl list sink-inputs short | cut -f1 | while read -r stream; do
         pactl move-sink-input "$stream" "$SINK_NAME"
     done
+
+    pactl set-sink-mute "$SINK_NAME" 0
 fi
