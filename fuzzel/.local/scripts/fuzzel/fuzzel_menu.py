54c965f8731ec5df4ff2b3d29f2968ae9df9b59c
sakievmi <sakievmi@proton.me>
2026-02-23 23:42:19 +0200

    fuzzel: py refactoring

--- fuzzel/.local/scripts/fuzzel/fuzzel_menu.py
+++ fuzzel/.local/scripts/fuzzel/fuzzel_menu.py
@@ -3,101 +3,107 @@ import subprocess
 from pathlib import Path
 
 
-class Fuzzel:
-    def __init__(self) -> None:
-        pass
+# Utils {{{
 
-    @staticmethod
-    def spawn_fuzzel(options_string: str, prompt: str) -> str:
-        executable = Path("/usr/bin/fuzzel")
 
-        process = subprocess.run(
-            [executable, "-d", "-p", prompt],
-            input=options_string,
-            capture_output=True,
-            text=True,
-        )
-        process_output = process.stdout
+def spawn_fuzzel(options_string: str, prompt: str) -> str:
+    executable = Path("/usr/bin/fuzzel")
 
-        return process_output
+    process = subprocess.run(
+        [executable, "-d", "-p", prompt],
+        input=options_string,
+        capture_output=True,
+        text=True,
+    )
+    process_output = process.stdout
 
-    # Classes
-    class FuzzelOption:
-        def __init__(self, title: str, command: list[str] | Fuzzel.Menu = []) -> None:
-            self.title = title
-            self.command = command
+    return process_output
 
-        def exec(self) -> Fuzzel.Menu | None:
-            if isinstance(self.command, list) and len(self.command) > 0:
-                subprocess.run(self.command, capture_output=True, text=True)
-                return None
 
-            if isinstance(self.command, Fuzzel.Menu):
-                return self.command
+# }}}
 
+# Classes {{{
+
+
+class FuzzelOption:
+    def __init__(self, title: str, command: list[str] | FuzzelMenu = []) -> None:
+        self.title = title
+        self.command = command
+
+    def exec(self) -> FuzzelMenu | None:
+        # Check if getting a command prompt
+        if isinstance(self.command, list) and len(self.command) > 0:
+            subprocess.run(self.command, capture_output=True, text=True)
             return None
 
-    class Menu:
-        def __init__(
-            self,
-            options: list[Fuzzel.FuzzelOption],
-            prompt: str = "󰍜 ",
-            has_back: bool = False,
-            parent: Fuzzel.Menu | None = None,
-        ) -> None:
-            self.options = options
-            self.prompt = prompt
-            self.has_back = has_back
-            self.parent = parent
+        # Check if getting a FuzzelMenu
+        if isinstance(self.command, FuzzelMenu):
+            return self.command
 
-        def show(self) -> Fuzzel.Menu | None:
-            local_options = self.options[:]
-            if self.has_back and isinstance(self.parent, Fuzzel.Menu):
-                local_options.insert(0, Fuzzel.FuzzelOption(" Back", self.parent))
+        return None
 
-            formatted_options = "".join(
-                [f"{option.title}\n" for option in local_options]
-            )
 
-            result = Fuzzel.spawn_fuzzel(formatted_options, self.prompt).split("\n")[0]
+class FuzzelMenu:
+    def __init__(
+        self,
+        options: list[FuzzelOption],
+        prompt: str = "󰍜 ",
+        has_back: bool = False,
+        parent: FuzzelMenu | None = None,
+    ) -> None:
+        self.options = options
+        self.prompt = prompt
+        self.has_back = has_back
+        self.parent = parent
 
-            if not result:
-                return None
+    def show(self) -> FuzzelMenu | None:
+        # Creating list copy with [:]
+        local_options = self.options[:]
+        if self.has_back and isinstance(self.parent, FuzzelMenu):
+            local_options.insert(0, FuzzelOption(" Back", self.parent))
 
-            for option in local_options:
-                if option.title == result:
-                    return option.exec()
+        # Formatting options for fuzzel
+        formatted_options = "".join([f"{option.title}\n" for option in local_options])
+
+        result = spawn_fuzzel(formatted_options, self.prompt).split("\n")[0]
+
+        # Check if user pressed esc
+        if not result:
             return None
 
+        for option in local_options:
+            if option.title == result:
+                return option.exec()
 
-# Menus
-# main_menu = Fuzzel.Menu()
-
-menu_main = Fuzzel.Menu(options=[])
-menu_power = Fuzzel.Menu(options=[], prompt="󰐥 ", has_back=True, parent=menu_main)
-
-menu_main.options = [
-    Fuzzel.FuzzelOption("󰣇 System"),
-    Fuzzel.FuzzelOption("  󰐥 Power Menu", menu_power),
-    Fuzzel.FuzzelOption("  󰚰 Update", ["echo", "done"]),
-]
-
-menu_power.options = [
-    Fuzzel.FuzzelOption("  󰐥 Shutdown", ["systemctl", "poweroff"]),
-    Fuzzel.FuzzelOption("   Reboot", ["systemctl", "reboot"]),
-]
+        return None
 
 
-def run_menu(start_menu: Fuzzel.Menu):
-    current_menu = start_menu
-    while current_menu is not None:
-        next_menu = current_menu.show()
+# }}}
 
-        if isinstance(next_menu, Fuzzel.Menu):
-            current_menu = next_menu
-        else:
-            break
+# Configuration {{{
 
 
-run_menu(menu_main)
+def setup_menus():
+    main = FuzzelMenu(options=[])
+    power = FuzzelMenu(options=[], prompt="󰐥 ", has_back=True, parent=main)
 
+    main.options = [
+        FuzzelOption("󰣇 System"),
+        FuzzelOption("  󰐥 Power Menu", power),
+        FuzzelOption("  󰚰 Update", ["echo", "done"]),
+    ]
+
+    power.options = [
+        FuzzelOption("  󰐥 Shutdown", ["systemctl", "poweroff"]),
+        FuzzelOption("   Reboot", ["systemctl", "reboot"]),
+    ]
+
+    return main
+
+
+# }}}
+
+if __name__ == "__main__":
+    current = setup_menus()
+    while current:
+        current = current.show()
