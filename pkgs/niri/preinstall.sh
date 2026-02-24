7a1fa63f0252ae28c0e7d7abd4df0d2e1b5853d8
sakievmi <sakievmi@proton.me>
2026-02-24 21:58:23 +0200

    merge niri and wallpapers

--- pkgs/niri/preinstall.sh
+++ pkgs/niri/preinstall.sh
@@ -1,2 +1,2 @@
-sudo pacman -S --noconfirm --needed niri xwayland-satellite xdg-desktop-portal-gnome xdg-desktop-portal-gtk noto-fonts noto-fonts-cjk
+sudo pacman -S --noconfirm --needed niri xwayland-satellite xdg-desktop-portal-gnome xdg-desktop-portal-gtk noto-fonts noto-fonts-cjk swww
 yay --diffmenu=false --cleanmenu=false --editmenu=false -S --noconfirm --needed ttf-twemoji-color bibata-cursor-theme
