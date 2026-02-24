b24227d53bae64ed5347c488273a566d6332e942
sakievmi <sakievmi@proton.me>
2026-02-24 23:26:01 +0200

    add install script for niri

---
+++ sdata/installers/niri.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+yay --diffmenu=false --cleanmenu=false --editmenu=false -S --noconfirm --needed ttf-twemoji-color bibata-cursor-theme niri xwayland-satellite xdg-desktop-portal-gnome xdg-desktop-portal-gtk noto-fonts noto-fonts-cjk swww
+
+# Icons
+mkdir -p ~/.local/share/icons/
+cd ~/.local/share/icons/
+
+git clone https://bitbucket.org/dirn-typo/yet-another-monochrome-icon-set.git yamis
+
+# Stow package
+cd ~/.dotfiles/pkgs
+stow -R -t ~ core/niri --adopt
