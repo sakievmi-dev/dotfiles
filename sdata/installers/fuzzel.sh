d9eed1224b62d52932d187ce745c1cd3ebe51c38
sakievmi <sakievmi@proton.me>
2026-02-24 23:31:47 +0200

    add shebans for installers

--- sdata/installers/fuzzel.sh
+++ sdata/installers/fuzzel.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 yay --diffmenu=false --cleanmenu=false --editmenu=false -S --noconfirm --needed cmd-polkit-git fuzzel jq
 
 # Stow package
--- sdata/installers/niri.sh
+++ sdata/installers/niri.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-yay --diffmenu=false --cleanmenu=false --editmenu=false -S --noconfirm --needed ttf-twemoji-color bibata-cursor-theme niri xwayland-satellite xdg-desktop-portal-gnome xdg-desktop-portal-gtk noto-fonts noto-fonts-cjk swww
+yay --diffmenu=false --cleanmenu=false --editmenu=false -S --noconfirm --needed ttf-twemoji-color bibata-cursor-theme niri xwayland-satellite xdg-desktop-portal-gnome xdg-desktop-portal-gtk noto-fonts noto-fonts-cjk swww ttf-jetbrains-mono-nerd
 
 # Icons
 mkdir -p ~/.local/share/icons/
