575f36ffcd6d2158b3511bfb52f8dceb4b0fb483
sakievmi <sakievmi@proton.me>
2026-02-25 15:10:35 +0200

    add installer for yazi

--- pkgs/core/yazi/preinstall.sh
+++
@@ -1,2 +0,0 @@
-sudo pacman -S --noconfirm --needed yazi neovim
-yay --diffmenu=false --cleanmenu=false --editmenu=false -S --noconfirm --needed xdg-desktop-portal-termfilechooser-hunkyburrito-git zen-browser-bin
